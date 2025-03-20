cd8d51c7713f8a880e1e6f67d4280173c04548c2
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-03-21 02:00:37 +0600

    Update index.js

--- index.js
+++ index.js
@@ -889,6 +889,27 @@ app.get("/fc", async (req, res) => {
 
 
 
+app.get('/alldl', async (req, res) => {
+  try {
+    const { url } = req.query;
+
+    if (!url) {
+      return res.status(400).json({ error: 'URL parameter is required' });
+    }
+
+  const response = await axios.get(`https://www.noobs-api.rf.gd/dipto/alldl?url=${url}`, {
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+      }
+    });
+    res.json({ data: response.data });
+  } catch (error) {
+    console.error('alldl Error:', error);
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`🔥 HASAN'S APIS IS RUNNING`);
