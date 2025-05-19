14a653e7c5b934a964f219c5f9b87a6f5c728413
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-05-19 15:00:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -170,16 +170,18 @@ app.get("/api/toxic-ai", async (req, res) => {
 
 app.get("/api/alldl", async (req, res) => {
     const url = req.query.url;
-    let format = req.query.format || "mp4";
+    let format = req.query.format || "b";
 
     if (!url) {
         return res.status(400).json({ status: "error", response: "URL is required", author: "♡︎ 𝐻𝐴𝑆𝐴𝑁 ♡︎" });
     };
 
-    if (!format === "mp3" || !format === "mp4") {
-        return res.json({ status: "error", response: "invalid format. Available: mp3 or mp4", author: "♡︎ 𝐻𝐴𝑆𝐴𝑁 ♡︎" });
-    }
-    
+    const formatMap = {
+        mp3: "bestaudio",
+        mp4: "b"
+    };
+
+    format = formatMap[format] || format;
 
     try {
         const { data } = await axios.get(`https://kingsoftoxiciter-production.up.railway.app/download?url=${encodeURIComponent(url)}&format=${format}`);
